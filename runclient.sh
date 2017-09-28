d9d896aa4871e5698af6e4e6064352252b9edc55
Rob <thesanch@gmail.com>
2017-09-28 00:37:21 -0400

    deletions.

--- runclient.sh
+++
@@ -1,4 +0,0 @@
-java -cp src/:build/:build/compute.jar \
-    -Djava.rmi.server.codebase=http://localhost/compute.jar \
-    -Djava.security.policy=client.policy \
-    client.ComputePi localhost 32
