169b3ab6b450e51fafe7d1adc90d624523e5a73b
mindonly <thesanch@gmail.com>
2017-10-01 20:54:21 -0400

    delete unused shell scripts.

--- bin/findprimes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-java -cp src:build:build/compute.jar \
-    -Djava.rmi.server.codebase=http://localhost:8080/build/ \
-    -Djava.rmi.server.hostname=localhost \
-    -Djava.security.policy=client.policy \
-    client.FindPrimes localhost 5 50 
